babfeffbc6702da6f899c301bdeda0574653cded
Rob Laughlin <waterfallkicks@gmail.com>
2024-07-25 03:48:39 -0700

    Add 4x4 grid

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const NUM_SQUARES = 16;
+
+function injectGrid(numSquares) {
+    let gridContainer = document.getElementById("GridContainer");
+    for (let i = 0; i < numSquares; i++) {
+        let square = document.createElement("div");
+        square.classList.add("gridSquare");
+
+        gridContainer.appendChild(square);
+    }
+}
+
+injectGrid(NUM_SQUARES);
