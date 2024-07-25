e7df47c5d1315dc055c429a430e10feb55fbe1c2
Rob Laughlin <waterfallkicks@gmail.com>
2024-07-25 04:25:35 -0700

    Implement resize grid functionality

--- js/index.js
+++ js/index.js
@@ -4,12 +4,38 @@ function changeBgColor(e) {
     e.target.classList.add("hovered");
 }
 
+function resizeGrid() {
+    let newGridSize = "";
+    while (
+        isNaN(newGridSize) 
+        || !Number.isInteger(Number(newGridSize)) 
+        || Number(newGridSize) < 1
+        || Number(newGridSize) > 100
+    ) {
+        newGridSize = prompt("Choose an integer N such that 1 <= N <= 100 to create an NxN grid.")
+    }
+    
+    const gridSize = Number(newGridSize)**2;
+
+    let gridContainer = document.getElementById("GridContainer");
+    let gridSquares = [];
+    for (let i = 0; i < gridSize; i++) {
+        let square = document.createElement("div");
+        square.classList.add("gridSquare");
+        square.addEventListener("mouseover", changeBgColor);
+        square.style.width = ((1 / Math.sqrt(gridSize)) * 100).toString() + '%';
+        gridSquares.push(square);
+    }
+    gridContainer.replaceChildren(...gridSquares);
+}
+
 function injectGrid(numSquares) {
     let gridContainer = document.getElementById("GridContainer");
     for (let i = 0; i < numSquares; i++) {
         let square = document.createElement("div");
         square.classList.add("gridSquare");
         square.addEventListener("mouseover", changeBgColor);
+        square.style.width = ((1 / Math.sqrt(numSquares)) * 100).toString() + '%';
         gridContainer.appendChild(square);
     }
 }
