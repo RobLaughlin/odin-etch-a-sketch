b96f2bddfca6893f60ecc5ede1a7d1d5705295b4
Rob Laughlin <waterfallkicks@gmail.com>
2024-07-25 03:56:15 -0700

    Add hover effect to grid squares

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 const NUM_SQUARES = 16*16;
 
+function changeBgColor(e) {
+    e.target.classList.add("hovered");
+}
+
 function injectGrid(numSquares) {
     let gridContainer = document.getElementById("GridContainer");
     for (let i = 0; i < numSquares; i++) {
         let square = document.createElement("div");
         square.classList.add("gridSquare");
-
+        square.addEventListener("mouseover", changeBgColor);
         gridContainer.appendChild(square);
     }
 }
